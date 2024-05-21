5d0254641f3eb33a364cdd69d9ba057d4ae0308b
SiveshMaharaj <sivesh.m710@gmail.com>
2024-05-21 13:22:14 +0200

    Repo test
    Repo test

--- ecomm-task/src/App.js
+++ ecomm-task/src/App.js
@@ -8,15 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Testing React App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p>        
       </header>
     </div>
   );
