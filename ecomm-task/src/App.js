2d7089c9d56d7e51f08fae5d40c9972b3d09e55e
SiveshMaharaj <sivesh.m710@gmail.com>
2024-05-21 15:07:49 +0200

    Updates to UI & packages
    Added in MUI, loader-spinner, images

--- ecomm-task/src/App.js
+++ ecomm-task/src/App.js
@@ -1,17 +1,47 @@
 import logo from './logo.svg';
-import './App.css';
+import './AppStyle.css';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing React App
-        </p>        
-      </header>
-    </div>
-  );
+  return(
+    <div className='App-welcome'>
+
+  <div className="font-head">    
+    <Card sx={{ width: '100%', borderRadius: 10, borderColor: 'coral', borderWidth: 10, borderStyle:'solid' }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 150, alignContent: 'center', justifyContent: 'center' }} color="coral" >
+        {'wantalot.com'}
+        </Typography>      
+      </CardContent>
+      
+    </Card>
+    {/* <MovingText type="typewriter"
+    dataText={['GoodStuff Gaming & Gadgets']}/>   */}   
+  </div> 
+      
+    <br></br>
+    {/* <img src={logo} className="App-logo" alt="logo" style={{height:'100px', width: '100px'}} /><br></br> */}
+    {/* <MovingText
+      type="fadeIn"
+      duration="1000ms"
+      delay="0s"
+      direction="normal"
+      timing="linear"
+      iteration="infinite"
+      fillMode="none"
+      >
+          <Typography  variant="h4" noWrap component="div">
+          <p className="font-head" >loading...</p>
+    </Typography>
+      </MovingText>   */}   
+    
+    </div>)
 }
 
 export default App;
