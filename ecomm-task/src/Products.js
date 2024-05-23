32ea57fb3a45d7a33b2d9d2a6ca2134b2cf0bc9f
SiveshMaharaj <sivesh.m710@gmail.com>
2024-05-23 10:15:55 +0200

    Temp filter
    Added temp filter for usage with data

--- ecomm-task/src/Products.js
+++ ecomm-task/src/Products.js
@@ -10,19 +10,45 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import { Divider } from '@mui/material';
 
 export default function Products() { 
+    const [category, setCategory] = React.useState('');
+
+    const handleCategory = (event) => {
+        setCategory(event.target.value);
+    };
   
  
 return (   
 <div className="App-header">    
-  <Card sx={{ width: '100', borderRadius: 10, alignContent: 'center', justifyContent: 'center', textAlign:'center' }}>
+  <Card sx={{ width: '100', borderRadius: 10, alignContent: 'center', justifyContent: 'center', textAlign:'center'}}>
     <CardContent>
       <Typography sx={{ fontSize: 30, alignContent: 'center', justifyContent: 'center' }} color="coral">
-        Products page
+        Products page                
       </Typography>         
     </CardContent>    
   </Card>
+  <br/>
+  <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Filter by category</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={category}
+          label="Filter by category"
+          onChange={handleCategory}
+        >
+          <MenuItem value={1}>All</MenuItem>
+          <MenuItem value={2}>Tech</MenuItem>
+          <MenuItem value={3}>Accessories</MenuItem>
+          <MenuItem value={4}>Clothes</MenuItem>
+        </Select>
+      </FormControl>
 </div>   
 )
 }
